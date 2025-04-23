8a508309aa35025b843ef9ad54c5c4dc2081f911
Henrique Galinha <hgalinha1999@hotmail.com>
2025-04-23 14:51:03 +0100

    Update useMovies.js

--- 07-usepopcorn/final/src/useMovies.js
+++ 07-usepopcorn/final/src/useMovies.js
@@ -1,60 +1,26 @@
-import { useState, useEffect } from "react";
+@media (max-width: 768px) {
+  .background {
+    /* transform: rotate(180deg); */
+  }
 
-const KEY = "f84fc31d";
+  .home-header {
+    /* transform: rotate(180deg); */
+    /* Mantém o resto das propriedades CSS de .home-header aqui, sem a rotação */
+  }
+}
 
-export function useMovies(query) {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
+.login-body {
+  margin-top: 35px;
+}
 
-  useEffect(
-    function () {
-      // callback?.();
-
-      const controller = new AbortController();
-
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
-
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.log(err.message);
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (query.length < 3) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-
-      fetchMovies();
-
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
-
-  return { movies, isLoading, error };
+.login-body,
+.login-form,
+.img-logo,
+.img-login,
+.login-frame,
+.user-password,
+.user-input,
+.social-login,
+.remember-credentials {
+  transition: all 0.3s ease-in-out;
 }
